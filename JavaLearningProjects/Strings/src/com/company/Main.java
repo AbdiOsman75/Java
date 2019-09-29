1320e85e8cc3dc8be3849093d00ac4ce780d2dfe
AbdiOsman75 <abdishakur.osman0@gmail.com>
2019-09-29 20:35:46 +0100

    String updated

--- JavaLearningProjects/Strings/src/com/company/Main.java
+++ JavaLearningProjects/Strings/src/com/company/Main.java
@@ -3,6 +3,22 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	// String is not a primitive but actually a class.
+    String myString = "This is a string";
+    System.out.println("myString is equal to "+ myString);
+    myString = myString + ", and this is more.";
+    System.out.println("myString is equal to "+ myString);
+    myString = myString + " \u00A9 2019";
+    System.out.println("myString is equal to "+ myString);
+
+    String numberString = "250.55";
+    numberString = numberString + "49.95";
+    System.out.println(numberString);
+
+    String laststring = "10";
+    int myInt = 50;
+    laststring = laststring + myInt;
+    System.out.println("LastString is equal to "+ laststring);
+
     }
 }
